58ba9afccbb36591ef4136c796087cd0aed02b26
bagus46 <32901956+bagus46@users.noreply.github.com>
2017-10-18 20:43:26 +0700

    Add files via upload

--- application/views/Login.php
+++ application/views/Login.php
@@ -53,19 +53,21 @@
     <h1 style="color: white; text-align: center;">Log In</h1>
         <form action="<?php echo base_url()."index.php/Login/aksi_login"; ?>" method="POST" class="form login">
         
-         <label for="login__username"><svg class="icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#user"></use></svg><span class="hidden">Username</span></label>
-               
           
 <div class="row">
                 	<div class="col-md-4 col-xs-4">
+                    <h4 style="color: white; text-align: center ;"><strong>Username</strong></h4>
                     </div> 
+
                     <div class="col-md-4 col-xs-4 form-group">
+
                        <input name="username" class="form-control " type="text" placeholder="username" required>
                     </div> 
                 </div>
-<label for="login__password"><svg class="icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#lock"></use></svg><span class="hidden">Password</span></label></br> 
+</br> 
                 <div class="row">
                 	<div class="col-md-4 col-xs-4">
+                     <h4 style="color: white; text-align: center;"><strong>Password</strong></h4>
                     </div>
                     <div class="col-md-4 col-xs-4 form-group">
                        <input name="password" class="form-control " type="password" placeholder="password" required>
@@ -82,7 +84,7 @@
 	    </div> <!-- /container -->
 	</div><!-- /headerwrap -->
 
-   < <link rel="stylesheet" href="<?php echo base_url()."assets/css/login-style.css" ?>" type="text/css">?>
+    <link rel="stylesheet" href="<?php echo base_url()."assets/css/login-style.css" ?>" type="text/css">
 
     <meta charset="utf-8">
     <meta name="robots" content="all,follow">
@@ -109,9 +111,44 @@
     <link href=<?php echo base_url()."css/custom.css"?> rel="stylesheet">
 
    <body>
+ <div id="footerwrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <h4>About</h4>
+          <div class="hline-w"></div>
+          <p>Website ini didirikan pada tahun 2017 oleh Tim Mahasiswa Sistem Informasi,
+ ITS Surabaya.</p>
+        </div>
+        <div class="col-lg-4">
+          <h4>Social Links</h4>
+          <div class="hline-w"></div>
+          <p>
+            <a href="www.google.com"><i class="fa fa-dribbble"></i></a>
+            <a href="www.facebook.com"><i class="fa fa-facebook"></i></a>
+            <a href="www.twitter.com"><i class="fa fa-twitter"></i></a>
+            <a href="www.instagram.com"><i class="fa fa-instagram"></i></a>
+            <a href="www.tumblr.com"><i class="fa fa-tumblr"></i></a>
+          </p>
+        </div>
+        <div class="col-lg-4">
+          <h4>Our Bunker</h4>
+          <div class="hline-w"></div>
+          <p>
+            Jalan Raya ITS,<br/>
+            Gedung Sistem Informasi<br/>
+            Sukolilo, Keputih, 60111<br/>
+          </p>
+        </div>
+      
+      </div><! --/row -->
+    </div><! --/container -->
+   </div><! --/footerwrap -->
+<div id="footerwrap2">
+    
+   </div><! --/footerwrap -->
 
-
-    <svg xmlns="http://www.w3.org/2000/svg" class="icons"><symbol id="arrow-right" viewBox="0 0 1792 1792"><path d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z"/></symbol><symbol id="lock" viewBox="0 0 1792 1792"><path d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z"/></symbol><symbol id="user" viewBox="0 0 1792 1792"><path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z"/></symbol></svg>
+  
 
 </body>
 </html>

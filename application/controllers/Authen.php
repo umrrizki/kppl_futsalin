7c7e70c5d5d17954f5c1ca8480d886dad55751d4
Unknown <umarrizki14@gmail.com>
2017-10-21 15:09:07 +0700

    commit

--- application/controllers/Authen.php
+++ application/controllers/Authen.php
@@ -10,7 +10,7 @@ class Authen extends CI_Controller{
 		
   $this->load->helper('url');
         
-        $this->load->database();
+        $this->load->database('futsalin');
 	}
  
 	function index(){

25e37fb829c01bf5c767c855797348d29540b52c
Unknown <baguspambudi96@gmail.com>
2017-10-23 13:03:02 +0700

    commit terakhir

--- application/tests/controllers/Logout_test.php
+++ application/tests/controllers/Logout_test.php
@@ -11,17 +11,9 @@ class Logout_test extends TestCase {
         $this->assertRedirect('Login');
         $this->assertFalse( isset($_SESSION['username']) );
 
-		//$output = $this->request('GET', 'Logout/keluar');
 		
-		//$this->assertTrue(isset($_SESSION['username'], $output));
 	}
         
-/*  public function test_keluar_direct()
-	{
-		 $_SESSION['username'] = "username";
-            $this->request('GET', 'Logout/keluar');
-            $this->assertRedirect('index.php/Login');
-	}*/
 
 }
 ?>
